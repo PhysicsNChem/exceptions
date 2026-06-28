b0e341d0a29b0c26c8da98bf8f8f6889677539a1
PhysicsNChem <rohanasharma1@gmail.com>
2026-06-28 13:12:54 -0400

    Added starting function

--- exceptions.cpp
+++ exceptions.cpp
@@ -3,9 +3,18 @@
 //June 28 2026
 
 #include <iostream>
+#include <exception>
+
+using namespace std;
+
+char character(char start, int offset);
 
 int main()
 {
     std::cout << "Hello World!\n";
+    cout << character('a', 1);
 }
 
+char character(char start, int offset) {
+    return start + offset;
+}
